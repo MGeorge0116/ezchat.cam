825fe8c5bfeeb898d925718657cb276984bb915f
Mathew George <MGeorge116@gmail.com>
2025-09-07 20:28:46 -0500

    fix(build): import RoomShell directly; remove RoomClient dependency

--- app/room/[username]/page.tsx
+++ app/room/[username]/page.tsx
@@ -1,10 +1,11 @@
-// app/room/[username]/page.tsx
 import { use } from "react";
-import RoomClient from "../../../components/room/RoomClient";
+import RoomShell from "../../../components/room/RoomShell";
 
 export default function RoomPage({
   params,
-}: { params: Promise<{ username: string }> }) {
+}: {
+  params: Promise<{ username: string }>;
+}) {
   const { username } = use(params);
-  return <RoomClient roomName={username} />;
+  return <RoomShell roomName={username} />;
 }
